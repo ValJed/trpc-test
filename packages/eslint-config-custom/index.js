e01d79ebc095c67eb3a59c16ad5b8d534900ed06
Jed <vjeudy@protonmail.com>
2023-03-14 23:10:56 +0100

    configs

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'turbo',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
